65ca3af169157c1a1c7aede490c961477507e4e2
A7medCherifi <businessahmedboss@gmail.com>
2026-07-01 14:41:06 +0000

    Organized the code perfectly

--- call_me_maybe.py
+++ call_me_maybe.py
@@ -1,20 +1,21 @@
 from src.parsing import parse_calling_function, parse_definition_function
 from src.manager import Manager
-from test import test_model
+from test import Model
 # from src.template import create_template
 
 
 def main():
-	manager = Manager()
-	prompts_calling = parse_calling_function('function_calling_tests.json')
-	definition_fn = parse_definition_function('functions_definition.json')
-	if not prompts_calling or not definition_fn:
-		exit(1)
-	manager.definition_functions = definition_fn
-	manager.prompts_calling = prompts_calling
-	# create_template(manager)
-	# print(manager.prompts_calling[0].prompt)
-	test_model(manager, "Replace all vowels in 'Programming is fun' with asterisks")
+    manager = Manager()
+    prompts_calling = parse_calling_function('function_calling_tests.json')
+    definition_fn = parse_definition_function('functions_definition.json')
+    if not prompts_calling or not definition_fn:
+        exit(1)
+    manager.definition_functions = definition_fn
+    manager.prompts_calling = prompts_calling
+    # create_template(manager)
+    # print(manager.prompts_calling[0].prompt)
+    model = Model(manager)
+    model.run_model("Replace all numbers in \"Hello 34 I'm 233 years old\" with NUMBERS")
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -10,152 +10,151 @@ class Model():
         self.model = Small_LLM_Model()
         self.functions_data = ""
         self.output_text = ""
-        self.input = ""
+        self.input_str = ""
         self.func_name = ""
+        self.parameters = ""
         self.current_token = ""
         self.manager = manager
         self.resources = dict()
         self.all_prompts = list()
 
-        self.prompt = f"""
-        Extract the function name and parameters as a valid JSON object. \
-        Functions: \
-        {self.functions_data} \
-        Example: \
-        Input text: What is the sum of 2 and 3? \
-        JSON output: \"name\": \"fn_add_numbers\", \"parameters\": {{"a": 2.0, "b": 3.0}}.\
-        Rules: \
-        1. If a parameter type is a Number cast it to a float. \
-        2. Output ONLY the raw JSON. \
-        Input Text: {self.input} \
-        JSON output: \
-        """
+        self.input_ids = None
+        self.extract_func_name = True
+        self.inject_par = False
+        self.injected = False
+
+        self.prompt = ""
 
     def _stage_of_prompt(self):
-        json_start = "{" + f'"prompt": "{self.input}", "name": "'
+        self.prompt = f"""
+        Functions Data: \
+            {self.functions_data} \
+        Extract the function name from Functions Data and parameters as a valid JSON object. \
+        Example: \
+            Input text: What is the sum of 2 and 3? \
+            JSON output: \"name\": \"fn_add_numbers\", \"parameters\": {{"a": 2.0, "b": 3.0}}.\
+        Rules: \
+            1. If a parameter type is a Number cast it to a float. \
+            2. Output ONLY the raw JSON. \
+        Input Text: {self.input_str} \
+        JSON output: \
+        """
+        json_start = "{" + f'"prompt": "{self.input_str}", "name": "'
         self.prompt += json_start
         self.output_text = json_start
 
+        print(self.functions_data)
+        tensor_ids = self.model.encode(self.prompt)
+        self.input_ids = tensor_ids[0].tolist()
 
 
     def _stage_of_name(self):
-        pass
+        if self.extract_func_name:
+            if '"' not in self.current_token:
+                self.func_name += self.current_token
+            else:
+                self.func_name += self.current_token.split('"')[0]
+                self.output_text += self.current_token
+                self.extract_func_name = False
 
 
+    def _stage_of_parameter(self, i):
+        if not self.inject_par:
+            inject_parameter_str = '"parameters": {'
+            parameter_tensor = self.model.encode(inject_parameter_str)
+            parameter_ids = parameter_tensor[0].tolist()
+            self.input_ids.extend(parameter_ids)
+            self.output_text += inject_parameter_str
+            self.injected = True
+            self.inject_par = True
 
-    def _stage_of_parameter(self):
-        pass
-
+        if self.inject_par:
+            if self.resources[self.func_name][i][1] == 'string':
+                par_str = f'"{self.resources[self.func_name][i][0]}": "'
+            else:
+                par_str = f'"{self.resources[self.func_name][i][0]}": '
+            par_tensor = self.model.encode(par_str)
+            par_ids = par_tensor[0].tolist()
+            self.input_ids.extend(par_ids)
+            self.output_text += par_str
+            self.injected = True
+            self.parameters += par_str
 
 
     def _extract_data_from_input(self):
         for fn in self.manager.definition_functions:
             self.functions_data += f"Name: {fn['name']} | Parameters: {fn['parameters']}\n"
             self.resources[fn['name']] = [(name, value['type']) for name, value in fn['parameters'].items()]
-
         for element in self.manager.prompts_calling:
             self.all_prompts.append(element['prompt'])
 
 
-
-
-
-    def run_model(self, manager, input):
+    def run_model(self, input_str):
+        self._extract_data_from_input()
         start = time.perf_counter()
-
-        inject_parameter_str = '"parameters": {'
-        parameter_tensor = self.model.encode(inject_parameter_str)
-        parameter_ids = parameter_tensor[0].tolist()
-
-        forced_after_comma = ' '
-        after_comma_tensor = self.model.encode(forced_after_comma)
-        after_comma_id = after_comma_tensor[0].tolist()
-
+        after_comma_id = self.model.encode(' ')[0].tolist()
 
         # for input in all_prompts:
-        self.input = input
-        
-        tensor_ids = self.model.encode(self.prompt)
-        input_ids = tensor_ids[0].tolist()
-
-        done_json = 0
-        open_braces = 1
-        closed_braces = 0
-        parameters = ""
+        # ===== Stage of Prompt ===== 
+        self.input_str = input_str
+        self._stage_of_prompt()
 
         i = 0
         next_arg = 1
-        extract_func_name= True
+        done_json = 0
+        open_braces = 1
+        closed_braces = 0
+
         while True:
-            logits = self.model.get_logits_from_input_ids(input_ids)
+            logits = self.model.get_logits_from_input_ids(self.input_ids)
             next_token_id = int(np.argmax(logits))
 
             self.current_token = self.model.decode([next_token_id])
 
             # check if the current has ',' forced injection ' ' to it
             if self.current_token.endswith(','):
-                self.current_token += forced_after_comma
-                input_ids.extend(after_comma_id)
+                self.current_token += ' '
+                self.input_ids.extend(after_comma_id)
 
             # keep counting braces if still json didnt finish
-            if not done_json:
-                self._stage_of_name()
-                open_braces += self.current_token.count("{")
-                closed_braces += self.current_token.count("}")
-                injected = False
+            open_braces += self.current_token.count("{")
+            closed_braces += self.current_token.count("}")
 
-                # extract function name and inject parameter str
-                if extract_func_name:
-                    if '"' not in self.current_token:
-                        self.func_name += self.current_token
-                    else:
-                        self.func_name += self.current_token.split('"')[0]
-                        extract_func_name = False
-                        output_text += self.current_token
+            self.injected = False
 
-                        input_ids.extend(parameter_ids)
-                        output_text += inject_parameter_str
+            # ===== Stage of Name ===== 
+            self._stage_of_name()
+            
+            # ===== Stage of Parameters ===== 
+            if not self.extract_func_name and i < len(self.resources[self.func_name]):
+                if next_arg:
+                    if not self.inject_par:
                         open_braces += 1
-                        injected = True
+                    self._stage_of_parameter(i)
+                    i += 1
+                    next_arg = 0
+                else:
+                    self.parameters += self.current_token
 
-                # inject the parameter keys
-                if 'parameters' in output_text and i < len(self.resources[self.func_name]):
-                    if next_arg:
-                        if self.resources[self.func_name][i][1] == 'string':
-                            par_str = f'"{self.resources[self.func_name][i][0]}": "'
-                        else:
-                            par_str = f'"{self.resources[self.func_name][i][0]}": '
-                            par_tensor = self.model.encode(par_str)
-                            par_ids = par_tensor[0].tolist()
+                if self.parameters.count(',') == i:
+                    next_arg = 1
 
-                            input_ids.extend(par_ids)
-                            output_text += par_str
-                            parameters += par_str
-                            i += 1
-                            next_arg = 0
-                            injected = True
+            # add the current token to the output text
+            if not self.injected:
+                self.input_ids.append(next_token_id)
+                self.output_text += self.current_token
 
-                    else:
-                        parameters += self.current_token
+            # check if the json done
+            if open_braces == closed_braces:
+                if self.inject_par:
+                    self.output_text, braces, _ = self.output_text.rpartition('}}')
+                    self.output_text += braces
+                    done_json = 1
 
-                    if parameters.count(',') == i:
-                        next_arg = 1
-
-                # add the current token to the output text
-                if not injected:
-                    input_ids.append(next_token_id)
-                    output_text += self.current_token
-
-                # check if the json done
-                if open_braces == closed_braces:
-                    if 'parameters' in output_text:
-                        output_text, braces, _ = output_text.rpartition('}}')
-                        output_text += braces
-                        done_json = 1
-
-            else:
+            if done_json:
                 break
-            print(output_text)
+
+            print(self.output_text)
 
         print("\n#################################################\n")
 
