a590e25f7265b97cb362c4fa0d59633a5d7894b8
root <buinessahmedboss@gmail.com>
2026-07-01 07:29:34 +0000

    Revert "First step in constrained decoding"
    This reverts commit a8205eae007e6266c469dfb256df9704976c2965.

--- call_me_maybe.py
+++ call_me_maybe.py
@@ -1,21 +1,20 @@
 from src.parsing import parse_calling_function, parse_definition_function
 from src.manager import Manager
-from test import Model
+from test import test_model
 # from src.template import create_template
 
 
 def main():
-  manager = Manager()
-  prompts_calling = parse_calling_function('function_calling_tests.json')
-  definition_fn = parse_definition_function('functions_definition.json')
-  if not prompts_calling or not definition_fn:
-    exit(1)
-  manager.definition_functions = definition_fn
-  manager.prompts_calling = prompts_calling
-  # create_template(manager)
-  # print(manager.prompts_calling[0].prompt)
-  model = Model()
-  model.test_model(manager, "Replace all vowels in 'Programming is fun' with asterisks")
+	manager = Manager()
+	prompts_calling = parse_calling_function('function_calling_tests.json')
+	definition_fn = parse_definition_function('functions_definition.json')
+	if not prompts_calling or not definition_fn:
+		exit(1)
+	manager.definition_functions = definition_fn
+	manager.prompts_calling = prompts_calling
+	# create_template(manager)
+	# print(manager.prompts_calling[0].prompt)
+	test_model(manager, "Replace all vowels in 'Programming is fun' with asterisks")
 
 
 if __name__ == "__main__":
