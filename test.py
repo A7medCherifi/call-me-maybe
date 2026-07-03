c97125712ae3fc360133eeace1504e43017f0199
A7medCherifi <businessahmedboss@gmail.com>
2026-07-03 11:49:57 +0100

    Fixed some issues, with improvement changes

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 import re
 import ast
 import time
@@ -21,23 +22,28 @@ class Model():
         self.vocab = dict()
 
         self.input_ids = None
+        self.const_prompt_ids = None
         self.extract_func_name = True
         self.inject_par = False
         self.injected = False
 
+    def _encode_constant_prompt(self):
+        const_prompt = f"""
+            Functions Data: \
+                {self.functions_data} \
+            Extract the function name from Functions Data and parameters as a valid JSON object. \
+            Example: \
+                Input text: What is the sum of 2 and 3? \
+                JSON output: \"name\": \"fn_add_numbers\", \"parameters\": {{"a": 2.0, "b": 3.0}}.\
+            Rules: \
+                1. If a parameter type is a Number cast it to a float. \
+                2. Output ONLY the raw JSON. \
+            Input Text: 
+        """
+        self.const_prompt_ids = self.model.encode(const_prompt)[0].tolist()
 
     def _stage_of_prompt(self):
-        self.prompt = f"""
-        Functions Data: \
-            {self.functions_data} \
-        Extract the function name from Functions Data and parameters as a valid JSON object. \
-        Example: \
-            Input text: What is the sum of 2 and 3? \
-            JSON output: \"name\": \"fn_add_numbers\", \"parameters\": {{"a": 2.0, "b": 3.0}}.\
-        Rules: \
-            1. If a parameter type is a Number cast it to a float. \
-            2. Output ONLY the raw JSON. \
-        Input Text: {self.input_str} \
+        self.prompt = f"""{self.input_str} \
         JSON output: \
         """
         json_start = "{" + f'"prompt": "{self.input_str}", "name": "'
@@ -45,7 +51,8 @@ class Model():
         self.output_text = json_start
 
         tensor_ids = self.model.encode(self.prompt)
-        self.input_ids = tensor_ids[0].tolist()
+        self.input_ids = copy.deepcopy(self.const_prompt_ids)
+        self.input_ids += tensor_ids[0].tolist()
 
 
     def _stage_of_name(self):
@@ -91,6 +98,7 @@ class Model():
 
     def _mask_unwanted_tokens(self, logits, found_name, valid_vocab):
         funcs_to_remove = []
+        matched = False
         while True:
             next_token_id = int(np.argmax(logits))
             if not valid_vocab:
@@ -120,84 +128,92 @@ class Model():
     def run_model(self, input_str):
         self._extract_data_from_input()
         start = time.time()
+        self._encode_constant_prompt()
         after_comma_id = self.model.encode(' ')[0].tolist()
 
         # ===== Stage of Prompt ===== 
-        self.input_str = input_str
-        self._stage_of_prompt()
+        for input in self.manager.prompts_calling:
+            self.input_str = input
+            self._stage_of_prompt()
 
-        i = 0
-        next_arg = 1
-        done_json = 0
-        open_braces = 1
-        closed_braces = 0
-        
-        matched = False
-        found_name = False
-        
-        valid_vocab = self.vocab
-        while True:
-            logits = self.model.get_logits_from_input_ids(self.input_ids)
+            i = 0
+            next_arg = 1
+            done_json = 0
+            open_braces = 1
+            closed_braces = 0
 
-            next_token_id, found_name, valid_vocab = self._mask_unwanted_tokens(
-                logits,
-                found_name,
-                valid_vocab
-                )
+            self.func_name = ""
+            self.parameters = ""
+
+            self.extract_func_name = True
+            self.inject_par = False
+            self.injected = False
+    
+            found_name = False
+            valid_vocab = copy.deepcopy(self.vocab)
+
+            while True:
+                logits = self.model.get_logits_from_input_ids(self.input_ids)
+
+                next_token_id, found_name, valid_vocab = self._mask_unwanted_tokens(
+                    logits,
+                    found_name,
+                    valid_vocab
+                    )
 
 
-            if found_name:
-                self.current_token = self.model.decode(next_token_id)
-            else:
-                self.current_token = self.model.decode([next_token_id])
+                if found_name:
+                    self.current_token = self.model.decode(next_token_id)
+                else:
+                    self.current_token = self.model.decode([next_token_id])
 
-            if self.current_token.endswith(','):
-                self.current_token += ' '
-                self.input_ids.extend(after_comma_id)
+                if self.current_token.endswith(','):
+                    self.current_token += ' '
+                    self.input_ids.extend(after_comma_id)
 
-            if not done_json:
-                open_braces += self.current_token.count("{")
-                closed_braces += self.current_token.count("}")
+                if not done_json:
+                    open_braces += self.current_token.count("{")
+                    closed_braces += self.current_token.count("}")
 
-                self.injected = False
+                    self.injected = False
 
-                # ===== Stage of Name ===== 
-                self._stage_of_name()
-                
-                # ===== Stage of Parameters ===== 
-                if not self.extract_func_name and i < len(self.resources[self.func_name]):
-                    if next_arg:
-                        if not self.inject_par:
-                            open_braces += 1
-                        self._stage_of_parameter(i)
-                        i += 1
-                        next_arg = 0
-                    else:
-                        self.parameters += self.current_token
+                    # ===== Stage of Name ===== 
+                    self._stage_of_name()
+                    
+                    # ===== Stage of Parameters ===== 
+                    if not self.extract_func_name and i < len(self.resources[self.func_name]):
+                        if next_arg:
+                            if not self.inject_par:
+                                open_braces += 1
+                            self._stage_of_parameter(i)
+                            i += 1
+                            next_arg = 0
+                        else:
+                            self.parameters += self.current_token
 
-                    if self.parameters.count(',') == i:
-                        next_arg = 1
+                        if self.parameters.count(',') == i:
+                            next_arg = 1
 
-                if not self.injected:
-                    if found_name:
-                        self.input_ids.extend(next_token_id)
-                        found_name = False
-                    else:
-                        self.input_ids.append(next_token_id)
-                    self.output_text += self.current_token
+                    if not self.injected:
+                        if found_name:
+                            self.input_ids.extend(next_token_id)
+                            found_name = False
+                        else:
+                            self.input_ids.append(next_token_id)
+                        self.output_text += self.current_token
 
-                if open_braces == closed_braces:
-                    if self.inject_par:
-                        self.output_text, braces, _ = self.output_text.rpartition('}}')
-                        self.output_text += braces
-                        done_json = 1
-            else:
-                break
+                    if open_braces == closed_braces:
+                        if self.inject_par:
+                            self.output_text, braces, _ = self.output_text.rpartition('}}')
+                            self.output_text += braces
+                            done_json = 1
+                else:
+                    break
 
-            print(self.output_text)
+                print(self.output_text)
 
-        print("\n#################################################\n")
-        
+            print("\n#################################################\n")
+            
         end = time.time()
         elapsed = end - start
         print(f"Time: {elapsed / 60:.2f}")
